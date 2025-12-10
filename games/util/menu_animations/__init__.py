bd480f5cc3c5088275f5a5d2ff8f52b80533c141
Kevin Balke <fughilli@gmail.com>
2025-12-10 01:16:37 -0800

    Fix menu animations

---
+++ games/util/menu_animations/__init__.py
@@ -0,0 +1,17 @@
+"""Menu animations package."""
+
+from games.util.menu_animations.animation_manager import MenuAnimationManager
+from games.util.menu_animations.cube_animation import CubeAnimation
+from games.util.menu_animations.plane_animation import PlaneAnimation
+from games.util.menu_animations.rainbow_animation import RainbowAnimation
+from games.util.menu_animations.sphere_animation import SphereAnimation
+from games.util.menu_animations.wave_animation import WaveAnimation
+
+__all__ = [
+    "MenuAnimationManager",
+    "CubeAnimation",
+    "PlaneAnimation",
+    "RainbowAnimation",
+    "SphereAnimation",
+    "WaveAnimation",
+]
